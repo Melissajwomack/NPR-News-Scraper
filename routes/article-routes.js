9c5ffcc71717f30cb85d3ad35eb6f271563cd047
Melissa Womack <m.womack010101@gmail.com>
2021-06-19 20:00:15 -0500

    fixing img issue

--- routes/article-routes.js
+++ routes/article-routes.js
@@ -37,6 +37,8 @@ router.get("/scrape", function (req, res) {
                 }
             });
         });
+        console.log(error);
+        console.log(response);
         res.redirect("/");
     });
 });
